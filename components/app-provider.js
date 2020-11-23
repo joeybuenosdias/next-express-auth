1fdc9f758412081476af64bd326c2d705516d0fc
Joey Schrader <jschrader@overstock.com>
2020-11-23 06:43:25 -0800

    removed linting comments

--- components/app-provider.js
+++ components/app-provider.js
@@ -2,11 +2,11 @@ import { createContext, useContext } from 'react';
 import PropTypes from 'prop-types';
 
 const AppContext = createContext();
-// eslint-disable-next-line import/prefer-default-export
 export const useAppContext = () => useContext(AppContext);
 
 export default function AppProvider({ value, children }) {
 	const newVal = {
+		user: {},
 		...value,
 	};
 
@@ -14,8 +14,6 @@ export default function AppProvider({ value, children }) {
 }
 
 AppProvider.propTypes = {
-	// eslint-disable-next-line react/forbid-prop-types
 	children: PropTypes.node.isRequired,
-	// eslint-disable-next-line react/forbid-prop-types
 	value: PropTypes.object.isRequired,
 };
